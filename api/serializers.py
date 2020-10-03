af19078f1224a9c03e107592e6f925085c020c55
Burak Kadir Er <ksenofanex@gmail.com>
2020-10-03 13:08:15 +0300

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,33 @@
 from rest_framework import serializers
-from .models import RawMaterial
+from .models import RawMaterial, Supplier, MeasurementType, Currency
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MeasurementType
+        fields = '__all__'
+
+
+class CurrencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Currency
+        fields = '__all__'
+
+
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = '__all__'
 
 
 class MaterialSerializer(serializers.ModelSerializer):
-    supplier_name = serializers.ReadOnlyField(source='supplier.name') # Instead of ID's, showing human-friendly names.
     currency_code = serializers.ReadOnlyField(source='currency.currency_code')
     measurement_type_name = serializers.ReadOnlyField(source='measurement_type.code')
-    stock_url = serializers.HyperlinkedIdentityField(view_name='api-stock-detail', lookup_field='pk') # Detail page.
+    stock_url = serializers.HyperlinkedIdentityField(view_name='api-stock-detail', lookup_field='pk')  # Detail page.
+    supplier_nested = SupplierSerializer(source='supplier', read_only=True)
 
     class Meta:
         model = RawMaterial
         fields = ('id', 'material_name', 'stock_url', 'measurement_type_name', 'measurement_type',
                   'total_amount', 'price', 'currency_code', 'currency',
-                  'created_date', 'updated_date', 'supplier_name', 'supplier',)
+                  'created_date', 'updated_date', 'supplier', 'supplier_nested',)
