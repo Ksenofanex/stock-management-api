653bce8eb665cc2e2035e71612219ed78ed17fa6
Burak Kadir Er <ksenofanex@gmail.com>
2021-05-24 18:04:22 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -11,15 +11,15 @@ from .views import \
 
 
 urlpatterns = [
-    path('materials/', MaterialList.as_view(), name='api-list'),
-    path('materials/<int:pk>/', MaterialDetail.as_view(), name='api-stock-detail'),
+    path('materials/', MaterialList.as_view(), name='stock-list'),
+    path('materials/<int:pk>/', MaterialDetail.as_view(), name='stock-detail'),
 
     path('suppliers/', SupplierList.as_view(), name='supplier-list'),
-    path('suppliers/<int:pk>', SupplierDetail.as_view(), name='api-supplier-detail'),
+    path('suppliers/<int:pk>', SupplierDetail.as_view(), name='supplier-detail'),
 
     path('currencies/', CurrencyList.as_view(), name='currency-list'),
-    path('currencies/<int:pk>', CurrencyDetail.as_view(), name='api-currency-detail'),
+    path('currencies/<int:pk>', CurrencyDetail.as_view(), name='currency-detail'),
 
     path('measurements/', MeasurementList.as_view(), name='measurement-list'),
-    path('measurements/<int:pk>', MeasurementDetail.as_view(), name='api-measurement-detail'),
+    path('measurements/<int:pk>', MeasurementDetail.as_view(), name='measurement-detail'),
 ]
