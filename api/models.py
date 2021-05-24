daebdd30c726988ee10b2a930845b29b8d3d5499
Burak Kadir Er <ksenofanex@gmail.com>
2021-05-24 18:58:44 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -3,15 +3,21 @@ from django.contrib.auth.models import User
 
 
 class RawMaterial(models.Model):
+    # Foreign Key Relations
     accountant = models.ForeignKey(User, on_delete=models.CASCADE,)
-    material_name = models.CharField(max_length=25)
+    supplier = models.ForeignKey('Supplier', blank=True, null=True, on_delete=models.CASCADE,)
     measurement_type = models.ForeignKey('MeasurementType', on_delete=models.CASCADE,)
+    currency = models.ForeignKey('Currency', on_delete=models.CASCADE,)
+    
+    # Core fields
+    material_name = models.CharField(max_length=25)
     total_amount = models.DecimalField(max_digits=9, decimal_places=2)
     price = models.DecimalField(max_digits=9, decimal_places=2)
-    currency = models.ForeignKey('Currency', on_delete=models.CASCADE,)
+    
+    # Date fields
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True, blank=True)
-    supplier = models.ForeignKey('Supplier', blank=True, null=True, on_delete=models.CASCADE,)
+
 
     def __str__(self):
         return self.material_name
