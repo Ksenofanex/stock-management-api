9b0ec2dd5f12967745faa3e91d307997fd362821
Burak Kadir Er <ksenofanex@gmail.com>
2021-11-27 12:41:22 +0300

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import RawMaterial, Currency, Supplier, MeasurementType
+
+from api.models import RawMaterial, Currency, Supplier, MeasurementType
 
 
 models = [RawMaterial, Currency, Supplier, MeasurementType]
