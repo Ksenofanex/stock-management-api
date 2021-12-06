2fc681e147f7531e0dac310ad38c8052cfcd570e
Ksenofanex <ksenofanex@gmail.com>
2021-12-06 14:17:02 +0300

    format admin module with Black

--- api/admin.py
+++ api/admin.py
@@ -54,20 +54,50 @@ class RawMaterialAdmin(admin.ModelAdmin):
 
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
-    list_display = ("name", "code",)
-    search_fields = ("name", "code",)
-    list_filter = ("name", "code",)
+    list_display = (
+        "name",
+        "code",
+    )
+    search_fields = (
+        "name",
+        "code",
+    )
+    list_filter = (
+        "name",
+        "code",
+    )
 
 
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
-    list_display = ("name", "phone", "email",)
-    search_fields = ("name", "phone", "email",)
-    list_filter = ("name", "phone", "email",)
+    list_display = (
+        "name",
+        "phone",
+        "email",
+    )
+    search_fields = (
+        "name",
+        "phone",
+        "email",
+    )
+    list_filter = (
+        "name",
+        "phone",
+        "email",
+    )
 
 
 @admin.register(MeasurementType)
 class MeasurementTypeAdmin(admin.ModelAdmin):
-    list_display = ("name", "code",)
-    search_fields = ("name", "code",)
-    list_filter = ("name", "code",)
+    list_display = (
+        "name",
+        "code",
+    )
+    search_fields = (
+        "name",
+        "code",
+    )
+    list_filter = (
+        "name",
+        "code",
+    )
